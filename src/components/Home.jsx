e52bd0bc56812373c5d4b9f80c1d8c9e6d01e360
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-10-16 02:51:26 +0100

    fetched reciped now showing in dom

---
+++ src/components/Home.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "../App.css";
+
+const Home = () => {
+  const [recipe, setRecipe] = useState([]);
+  useEffect(() => {
+    getRecipe();
+  }, []);
+  const getRecipe = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=12`
+    );
+    const data = await api.json();
+    console.log(data);
+    setRecipe(data.recipes);
+  };
+  return (
+    <div>
+      <h1>RecipePalace</h1>
+      <div className="container">
+        {recipe.map((recipe) => {
+          return (
+            <div className="movie" key={recipe.id}>
+              <div>
+                <p>{recipe.title}</p>
+              </div>
+              <div>
+                <img src={recipe.image} alt="" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
