d1c49c2141d3b4c31a1c559ea8004621f5f65941
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-10-19 19:25:05 +0100

    search section in progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Home from "./components/Home";
 import "./App.css";
+import SearchIcon from "./search.svg";
 
-function App() {
+const App = () => {
+  const [recipe, setRecipe] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSubmit = (e) => {
+    
+  }
+
+  useEffect(() => {
+    getRecipe();
+  }, []);
+
+  const getRecipe = async (title) => {
+    const check = localStorage.getItem("recipe");
+    if (check) {
+      setRecipe(JSON.parse(check));
+
+    }
+    else{
+
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=12`
+      );
+
+      const data = await api.json();
+      localStorage.setItem("recipe", JSON.stringify(data.recipes));
+      console.log(data);
+      setRecipe(data.recipes);
+    }
+  };
   return (
     <div className="App">
       <h1>RecipePalace</h1>
-      <Home />
+      <div className="search">
+        <input
+          type="text"
+          value={searchTerm}
+          placeholder="Search for recipes"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => setRecipe(searchTerm)}
+        />
+      </div>
+      {recipe?.length > 0 ? (
+        <div className="container">
+          {recipe.map((recipe) => (
+            <Home recipe={recipe} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2> No recipe found</h2>
+        </div>
+      )}
+      ;
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,27 @@
 import React from "react";
-import { useState, useEffect } from "react";
+
 import "../App.css";
 
-const Home = () => {
-  const [recipe, setRecipe] = useState([]);
-  useEffect(() => {
-    getRecipe();
-  }, []);
-  const getRecipe = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=12`
-    );
-    const data = await api.json();
-    console.log(data);
-    setRecipe(data.recipes);
-  };
+const Home = ({recipe}) => {
+ 
+ 
+ 
   return (
     <div>
-      
-      <div className="container">
-        {recipe.map((recipe) => {
-          return (
-            <div className="movie" key={recipe.id}>
-              <div>
-                <img src={recipe.image} alt="" />
-              </div>
-              <div>
-                <p>{recipe.title}</p>
-              </div>
-            </div>
-          );
-        })}
+      {/* <div className="container"> */}
+
+      {/* return ( */}
+      <div className="movie" key={recipe.id}>
+        <div>
+          <img src={recipe.image} alt="" />
+        </div>
+        <div>
+          <p>{recipe.title}</p>
+        </div>
       </div>
+      {/* ); */}
+      {/* })} */}
+      {/* </div> */}
     </div>
   );
 };
