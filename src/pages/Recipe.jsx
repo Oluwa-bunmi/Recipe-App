e6ff5306ab567743a03caabb1d5aa0b364b601a2
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-10-26 21:47:40 +0100

    information display on the recipe page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -6,23 +6,50 @@ import "../App.css";
 const Recipe = () => {
   let params = useParams();
   const [details, setDetails] = useState({});
+  const [activeTab, setActiveTab] = useState("instructions");
 
   const fetchDetails = async () => {
     const data = await fetch(
       `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
     );
     const recipeData = await data.json();
+    console.log(recipeData);
     setDetails(recipeData);
   };
   useEffect(() => {
     fetchDetails();
-  },[params.name])
-  return <div className="wrapper">
-    <div>
+  }, [params.name]);
+  return (
+    <div className="wrapper">
+      <div>
         <h2>{details.title}</h2>
+        <img src={details.image} alt="" />
+      </div>
+      <div className="info">
+        <button
+          className={activeTab === "instructions" ? "active" : ""}
+          onClick={() => setActiveTab("instructions")}
+        >
+          Instructions
+        </button>
+        <button
+          className={activeTab === "ingredients" ? "active" : ""}
+          onClick={() => setActiveTab("ingredients")}
+        >
+          Ingredients
+        </button>
+        <div>
+            <h3 dangerouslySetInnerHTML={{__html: details.summary}}></h3>
+            <h3 dangerouslySetInnerHTML={{__html: details.instructions}}></h3>
+        </div>
+      </div>
+      <ul>
+        {details.extendedIngredients.map((ingredient) => (
+            <li key={ingredient.id}>{ingredient.original}</li>
+        ) )}
+      </ul>
     </div>
-    
-    </div>;
+  );
 };
 
 export default Recipe;
