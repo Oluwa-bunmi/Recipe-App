578eeee912389f50c37ecfd781c348f3690c419f
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-10-26 20:25:43 +0100

    ...setting up the full recipe page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -3,7 +3,16 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const Recipe = () => {
-    let params = useParams();
+  let params = useParams();
+  const [details, setDetails] = useState({});
+
+  const fetchDetails = async () => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+    );
+    const recipeData = await data.json();
+    setDetails(recipeData);
+  };
   return <div>Recipe</div>;
 };
 
