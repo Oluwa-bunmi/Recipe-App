104e08cf8687e74b3f608d241e1d37d290925c70
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-10-26 12:59:44 +0100

    new method working, fetched recipes now on screen

--- src/App.js
+++ src/App.js
@@ -7,34 +7,34 @@ import { BrowserRouter } from "react-router-dom";
 import Pages from "./pages/Pages";
 
 const App = () => {
-  const [recipe, setRecipe] = useState([]);
+  // const [recipe, setRecipe] = useState([]);
 
-  const getRecipe = async () => {
-    // const check = localStorage.getItem("recipe");
-    // if (check) {
-    //   setRecipe(JSON.parse(check));
-    // } else {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=12`
-    );
+  // const getRecipe = async () => {
+  //   // const check = localStorage.getItem("recipe");
+  //   // if (check) {
+  //   //   setRecipe(JSON.parse(check));
+  //   // } else {
+  //   const api = await fetch(
+  //     `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=12`
+  //   );
 
-    const data = await api.json();
-    // localStorage.setItem("recipe", JSON.stringify(data.recipes));
-    console.log(data);
-    setRecipe(data.recipes);
-    // }
-  };
+  //   const data = await api.json();
+  //   // localStorage.setItem("recipe", JSON.stringify(data.recipes));
+  //   console.log(data);
+  //   setRecipe(data.recipes);
+  //   // }
+  // };
 
-  useEffect(() => {
-    getRecipe();
-  }, []);
+  // useEffect(() => {
+  //   getRecipe();
+  // }, []);
 
   return (
     <div className="App">
-      <BrowserRouter>
-        {/* <h1>RecipePalace</h1> */}
+      {/* <BrowserRouter> */}
+        <h1>RecipePalace</h1>
         <Search icon={SearchIcon} />
-        <Home/>
+        <Home />
         {/* <{recipe?.length > 0 ? (
           <div className="container" key={recipe.id}>
             {recipe.map((recipe) => (
@@ -47,7 +47,7 @@ const App = () => {
           </div>
         )}>
         ; */}
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
 import "../App.css";
 
-const Home = ({recipe}) => {
+const Home = () => {
+  const [recipe, setRecipe] = useState([]);
  
- 
+  const getRecipe = async () => {
+    // const check = localStorage.getItem("recipe");
+    // if (check) {
+    //   setRecipe(JSON.parse(check));
+    // } else {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=12`
+    );
+
+    const data = await api.json();
+    // localStorage.setItem("recipe", JSON.stringify(data.recipes));
+    console.log(data);
+    setRecipe(data.recipes);
+    // }
+  };
+
+  useEffect(() => {
+    getRecipe();
+  }, []);
+
  
   return (
     <div>
@@ -22,26 +42,20 @@ const Home = ({recipe}) => {
       {/* ); */}
       {/* })} */}
       {/* </div> */}
-
-      {recipe.map((recipe) => {
-      return(
-        <div className="container" key={
-          recipe.id
-        }>
-          <div className="movie">
-             <div>
-          <img src={recipe.image} alt="" />
-        </div>
-         <div>
-          <p>{recipe.title}</p>
-        </div>
-            
-          </div>
-        </div>
-      )
-     })}
-
-
+      <div className="container">
+        {recipe.map((recipe) => {
+          return (
+            <div className="movie" key={recipe.id}>
+              <div>
+                <img src={recipe.image} alt="" />
+              </div>
+              <div>
+                <p>{recipe.title}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
